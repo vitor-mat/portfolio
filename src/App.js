370a792bf92b1ac3988d4f7f8265d2f80d8fbd30
vitor-mat <vitormateusalves123@gmail.com>
2021-09-20 18:11:59 -0300

    fonte instalada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import { GlobalStyle } from './style/GlobalStyle'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <h1>Ola mundo</h1>
+    <Router>
+          <GlobalStyle/>
+          <Switch>
+            <Route exact path="/">
+              <h1>Ola mundo</h1>
+            </Route>
+          </Switch>
+    </Router>
   );
 }
 
