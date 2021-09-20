7f095658645eb68ca25d65c910737943ba8e4fec
vitor-mat <vitormateusalves123@gmail.com>
2021-09-20 18:28:33 -0300

    style global adicionando

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -2,6 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        overflow-x: hidden;
         font-family: 'Roboto Slab', serif;
     }
 `
