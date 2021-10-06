cba05dcf0ad77ca5e07e81f363356ed221ee5bda
vitor-mat <vitormateusalves123@gmail.com>
2021-10-06 18:06:43 -0300

    descrição do projeto da netflix adicionado

--- src/db/project_list.js
+++ src/db/project_list.js
@@ -6,7 +6,7 @@ export const projectList = [
         indice: "001",
         image: netflixCloneImg,
         link: "https://vitor-mat.github.io/Clone-Netlix-Page---HTML-CSS-e-JAVASCRIPT/",
-        description: "#",
-        tags: [],
+        description: "Este projeto trata-se de um clone da interface do netflix. É um projeto com o único intuito de demonstrar as minhas habilidades com html, css e javascript.",
+        tags: ["#html", "#css", "#javacript"],
     },
 ]
