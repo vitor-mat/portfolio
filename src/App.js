5f81fb478e4175fe3c2023c2acec3a9b388f3964
vitor-mat <vitormateusalves123@gmail.com>
2021-10-21 15:43:48 -0300

    rotas voltaram a funcionar + reinstalei as dependencias

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ import { Projects } from './pages/projects/index';
 function App() {
   return (
     <Router>
-          <GlobalStyle/>
-          <Switch>
-            <Route exact path="/">
-              <Home></Home>
-            </Route>
-            <Route path="/home">
-              <Home></Home>
-            </Route>
-            <Route path="/projects">
-              <Projects></Projects>
-            </Route>
-          </Switch>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/projects">
+          <Projects></Projects>
+        </Route>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
     </Router>
   );
 }
