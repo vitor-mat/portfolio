1cb8c5fabc85a07687446cb8f11723e33b6de0f7
vitor-mat <vitormateusalves123@gmail.com>
2021-11-08 17:32:25 -0300

    opcoes do menu mobile aparecendo quando clicamos no menu hamburguer

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ let test = true
 document.getElementById("menu-mobile").addEventListener("click", () => {
 
     if(test){
+
         document.getElementById("hamburgue-1").classList.add("hamburguer-activated-1")
         document.getElementById("hamburgue-2").classList.add("hamburguer-activated-2")
         document.getElementById("hamburgue-3").classList.add("hamburguer-activated-3")
         
+        document.querySelector(".menu-mobile-list").classList.add("menu-mobile-list-activated")
+
         test = false
     }else{
         document.getElementById("hamburgue-1").classList.remove("hamburguer-activated-1")
         document.getElementById("hamburgue-2").classList.remove("hamburguer-activated-2")
         document.getElementById("hamburgue-3").classList.remove("hamburguer-activated-3")    
+
+        document.querySelector(".menu-mobile-list").classList.remove("menu-mobile-list-activated")
 
         test= true
     }
